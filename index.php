8b6344ef2ee70552687f85f41b89345739c29251
Maciej Rybak <maciek.rybak95@gmail.com>
2022-12-03 12:50:20 +0100

    Add condition if

--- index.php
+++ index.php
@@ -1,16 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Demo</title>
-  </head>
-  <body>
-    <h1> 
-      <?php
-        $greeting="Hello";
-        echo "$greeting Everybody!";
-       ?>
-    </h1>
-    
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Demo</title>
+  <style>
+    body {
+      display: grid;
+      place-items: center;
+      height: 100vh;
+      margin: 0;
+      font-family: sans-serif;
+    }
+  </style>
+</head>
+
+<body>
+
+  <?php
+  $name = "Dark Matter";
+  $read = true;
+
+  if ($read) {
+    $message = "You have read $name";
+  } else {
+    $message = "You have not read $name";
+  }
+
+  ?>
+
+  <h1>
+    <?php echo $message ?>
+    <?= $message ?>
+  </h1>
+
+
+</body>
+
 </html>
