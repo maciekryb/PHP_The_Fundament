abf08876b5ff156a08e04cab86877dff3079db70
Maciej Rybak <maciek.rybak95@gmail.com>
2022-12-07 23:09:36 +0100

    Function

--- index.php
+++ index.php
@@ -20,19 +20,40 @@
       'author' => 'Andy Weir',
       'releaseYear' => 2021,
       'purchaseUrl' => 'http://example.com'
-    ]
+    ],
+    [
+      'name' => 'The Martian',
+      'author' => 'Andy Weir',
+      'releaseYear' => 2011,
+      'purchaseUrl' => 'http://example.com'
+    ],
   ];
+
+  function filterByAuthor($books, $author)
+  {
+    $filteredBooks = [];
+
+    foreach ($books as $book) {
+      if ($book['author'] === $author) {
+        $filteredBooks[] = $book;
+      }
+    }
+    return $filteredBooks;
+  }
+
   ?>
 
   <ul>
-    <?php foreach ($books as $book) : ?>
+    <?php foreach (filterByAuthor($books, 'Philip K. Dick') as $book) : ?>
       <li>
         <a href="<?= $book['purchaseUrl'] ?>">
-          <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
+          <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)- By <?= $book['author'] ?>
         </a>
       </li>
     <?php endforeach; ?>
   </ul>
+
 </body>
+
 
 </html>
