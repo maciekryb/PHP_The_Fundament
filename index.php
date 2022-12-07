317b311feeefb41eb975a94f7b1470b6535dc1c5
Maciej Rybak <maciek.rybak95@gmail.com>
2022-12-07 22:44:27 +0100

    fix code

--- index.php
+++ index.php
@@ -1,41 +1,38 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
+  <meta charset="UTF-8">
   <title>Demo</title>
 </head>
 
 <body>
-
   <?php
   $books = [
     [
-      'name' => 'Do Androids Dream of Electris Shepa',
+      'name' => 'Do Androids Dream of Electric Sheep',
       'author' => 'Philip K. Dick',
-      'purchaseUrl' => 'http://example.com',
-      'releaseYear' => '2021'
+      'releaseYear' => 1968,
+      'purchaseUrl' => 'http://example.com'
     ],
     [
-      'name' => "Hail Mary",
+      'name' => 'Project Hail Mary',
       'author' => 'Andy Weir',
-      'purchaseUrl' => 'http://example2.com',
-      'releaseYear' => '201'
+      'releaseYear' => 2021,
+      'purchaseUrl' => 'http://example.com'
     ]
   ];
   ?>
 
-  <p>
+  <ul>
     <?php foreach ($books as $book) : ?>
-
       <li>
         <a href="<?= $book['purchaseUrl'] ?>">
           <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
         </a>
       </li>
-    <?php endforeach ?>
-  </p>
-
+    <?php endforeach; ?>
+  </ul>
 </body>
 
 </html>
