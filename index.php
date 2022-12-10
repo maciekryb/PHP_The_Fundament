3c2e59efc75a5f19e98d14985c1fc4258d6e6308
Maciej Rybak <maciek.rybak95@gmail.com>
2022-12-10 12:45:10 +0100

    Array filter

--- index.php
+++ index.php
@@ -30,20 +30,10 @@
   ];
 
 
-  function filter($items, $fn)
-  {
-    $filteredItems = [];
 
-    foreach ($items as $item) {
-      if ($fn($item)) {
-        $filteredItems[] = $item;
-      }
-    }
-    return $filteredItems;
-  }
 
-  $filteredBooks = filter($books, function ($book) {
-    return $book['releaseYear'] < 2000;
+  $filteredBooks = array_filter($books, function ($book) {
+    return $book['author'] === 'Philip K. Dick';
   });
 
   ?>
