2f6eb0eb1d32e0b58a4aeb87da908cbd19bce988
Maciej Rybak <maciek.rybak95@gmail.com>
2023-08-06 20:06:14 +0200

    add basic router

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+require "functions.php";
 
-echo 'hellop';
+// dd($_SERVER);
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+
+
+if ($uri === '/') {
+    require 'controllers/index.php';
+} else if ($uri === '/about') {
+    require 'controllers/about.php';
+} else if ($uri === '/contact') {
+    require 'controllers/contact.php';
+}
