a61ab759a5f040e5b3c3edae2110e4a9a791ae69
Maciej Rybak <maciek.rybak95@gmail.com>
2023-08-08 00:48:25 +0200

    shortcut to classess

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
+
 require "functions.php";
-require "router.php";
+// require "router.php";
+
+class Person
+{
+    public $name;
+    public $age;
+
+    public function breathe()
+    {
+        echo $this->name . " is breathing";
+    }
+}
+
+$person = new Person();
+
+$person->name = 'John Doe';
+$person->age = 25;
+
+dd($person->breathe());
