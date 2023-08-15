088aaa2e6ef97f7ee637ff81315e7daf345bf619
Maciej Rybak <maciek.rybak95@gmail.com>
2023-08-15 18:33:56 +0200

    add notes page

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 
 require "functions.php";
 require "Database.php";
+require "router.php";
 
 $config = require('config.php');
 $db = new Databse($config['database']);
 
-$id = $_GET['id'];
+// $id = $_GET['id'];
 
-$query = "select * from posts where id = :id";
+// $query = "select * from posts where id = :id";
 // dd($query);
 
-$posts = $db->query($query, [$id])->fetch();
+// $posts = $db->query($query, [$id])->fetch();
 
-dd($posts);
+// dd($posts);
