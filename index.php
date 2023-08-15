077eec167ede5033eb010bb3be0f3eeeebac3268
Maciej Rybak <maciek.rybak95@gmail.com>
2023-08-15 15:36:33 +0200

    small code refactor

--- index.php
+++ index.php
@@ -5,10 +5,13 @@ require "functions.php";
 
 $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
 $pdo = new PDO($dsn);
-$statment = $pdo->prepare("select * from posts");
+// $statment = $pdo->prepare("select * from posts");
+$statment = $pdo->prepare("select * from posts where id=1");
 $statment->execute();
+
 $posts = $statment->fetchAll(PDO::FETCH_ASSOC);
 
+echo $posts;
 foreach ($posts as $post) {
     echo "<li>" . $post['title'] . "</li>";
 }
