a5eef4908034def0428d88d1007e2f7ce153fb5b
Maciej Rybak <maciek.rybak95@gmail.com>
2023-08-16 21:10:15 +0200

    add Response class

--- controllers/note.php
+++ controllers/note.php
@@ -4,17 +4,18 @@ $config = require('config.php');
 $db = new Databse($config['database']);
 
 $heading = 'Note';
-
+$currentUserId = '1';
 
 $note = $db->query('select * from notes where id = :id ', ['id' => $_GET['id']])->fetch();
 
 if (!$note) {
     abort();
 }
-// dd($note);
 
-if ($note['user_id'] !== '1') {
-    abort(403);
+
+if ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
 }
 
+// dd($note);
 require "views/note.view.php";
