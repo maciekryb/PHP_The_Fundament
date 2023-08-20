d80830bae5e5a20ed1984615183ac11c06290afe
Maciej Rybak <maciek.rybak95@gmail.com>
2023-08-20 22:05:52 +0200

    create validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,21 +1,24 @@
 <?php
+require 'Validator.php';
 
-$config = require('config.php');
+$config = require 'config.php';
 $db = new Databse($config['database']);
 
 $heading = 'Create Note';
+
+// if (!Validator::email('fsdfsdfsdfs@o2.@pl')) {
+//     dd("that's not valid email");
+// };
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $errors = [];
 
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = 'A body is required';
+
+    if (!Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1000 characters is required';
     }
 
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'A body can not be more than 1000 characters';
-    }
 
     if (empty($errors)) {
         $db->query('INSERT INTO notes (body, user_id) VALUES(:body, :user_id)', [
