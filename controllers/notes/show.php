e3ca4fcb3cdfb8d96310ab786a7750117a04376f
Maciej Rybak <maciek.rybak95@gmail.com>
2023-08-22 21:56:02 +0200

    add resourceful naming convention

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -11,4 +11,4 @@ $note = $db->query('select * from notes where id = :id ', ['id' => $_GET['id']])
 authorize($note['user_id'] == $currentUserId);
 
 // dd($note);
-require "views/note.view.php";
+require "views/notes/show.view.php";
