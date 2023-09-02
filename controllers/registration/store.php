14b911df6217b8da4547ed3467acdf60501ca1b1
Maciej Rybak <maciek.rybak95@gmail.com>
2023-09-02 14:44:58 +0200

    register a new user

---
+++ controllers/registration/store.php
@@ -0,0 +1,49 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+
+$errors = [];
+
+if (!Validator::email($email)) {
+    $errors['email'] = 'please provide a valid email adress';
+}
+
+if (!Validator::string($password, 7, 255)) {
+    $errors['password'] = 'please provide a password at least 7 characters';
+}
+
+if (!empty($errors)) {
+    return view("registration/create.view.php", [
+        'errors' => $errors,
+    ]);
+}
+
+$db = App::resolve(Database::class);
+
+$user = $db->query('select * from users where email =:email', ['email' => $email])->find();
+
+
+if ($user) {
+
+    header('location: /notes');
+    exit();
+} else {
+
+    $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
+        'email' => $email,
+        'password' => $password,
+    ]);
+
+    $_SESSION['user'] = ['email' => $email];
+
+    header('location: /');
+    exit();
+}
