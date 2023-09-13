37d7b9c763c18bd77f9112310b5b24873ce42f0b
Maciej Rybak <maciek.rybak95@gmail.com>
2023-09-14 00:06:23 +0200

    Login refactor

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -2,23 +2,40 @@
 
 namespace Http\Forms;
 
+use Core\ValidationException;
 use Core\Validator;
 
 class LoginForm
 {
+    public array $attributes;
     protected $errors = [];
 
-    public function validate($email, $password)
+    public function __construct($attributes)
     {
-        if (!Validator::email($email)) {
+        if (!Validator::email($attributes['email'])) {
             $this->errors['email'] = 'Please provide a valid email address.';
         }
 
-        if (!Validator::string($password, 1, 255)) {
+        if (!Validator::string($attributes['password'])) {
             $this->errors['password'] = 'Please provide a valid password.';
         }
+    }
 
-        return empty($this->errors);
+    public static function validate($attributes)
+    {
+        $instance = new static($attributes);
+
+        return $instance->failed() ? $instance->throw() : $instance;
+    }
+
+    public function throw()
+    {
+        ValidationException::throw($this->errors(), $this->attributes);
+    }
+
+    public function failed()
+    {
+        return count($this->errors);
     }
 
     public function errors()
@@ -29,5 +46,7 @@ class LoginForm
     public function error($field, $message)
     {
         $this->errors[$field] = $message;
+
+        return $this;
     }
 }
--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -1,23 +1,23 @@
 <?php
 
 use Core\Authenticator;
-use Core\Session;
 use Http\Forms\LoginForm;
 
+$form = LoginForm::validate($attributes = [
+    'email' => $_POST['email'],
+    'password' => $_POST['password']
+]);
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+$signedIn = (new Authenticator)->attempt(
+    $attributes['email'],
+    $attributes['password']
+);
 
-$form = new LoginForm();
-
-if ($form->validate($email, $password)) {
-
-    if ((new Authenticator)->attempt($email, $password)) {
-        redirect('/');
-    }
-    $form->error('email', 'No matching account found for that email adress and password');
+if (!$signedIn) {
+    $form->error(
+        'email',
+        'No matching account found for that email address and password.'
+    )->throw();
 }
 
-Session::flash('errors', $form->errors());
-
-return redirect('/login');
+redirect('/');
