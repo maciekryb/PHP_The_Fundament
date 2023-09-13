be68215815b4d8fec50ee535ed33d5338e4d7dff
Maciej Rybak <maciek.rybak95@gmail.com>
2023-09-14 00:15:05 +0200

    add php errors in browser

--- public/index.php
+++ public/index.php
@@ -5,6 +5,10 @@ use Core\ValidationException;
 
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 const BASE_PATH = __DIR__ . '/../';
 
